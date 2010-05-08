eaac4a2732967724b223f5ad1264c16d2d6ba232
Sinis <m.siniss@gmail.com>
2010-05-08 16:26:19 +0200

    Player moving fixed.

--- src/meneland.cpp
+++ src/meneland.cpp
@@ -2,7 +2,7 @@
 // Projekt: Meneland
 // Autor: Sinis
 // Data utworzenia: 5.04.2010
-// Data modyfikacji: 14.04.2010
+// Data modyfikacji: 8.05.2010
 // Opis: Implementacja głównej klasy gry.
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -55,7 +55,10 @@ int Meneland::Execute()
 	// Na początku tworzy Mapę, Playera i Camerę. Ustawia playerowi miejsce
 	// na planszy i przechodzi do głównej pętli.
 	int playerX, playerY;
-	Uint8* keystate = SDL_GetKeyState(0);
+	SDL_Event event;
+	Direction dir; // Kierunek ruchu.
+	bool moving = false; // Czy gracz jest w ruchu?
+	//Uint8* keystate = SDL_GetKeyState(0);
 	bool quit = false;
 	Map* map = new Map(playerX, playerY);
 	Player* player = new Player(playerX, playerY, map);
@@ -68,14 +71,35 @@ int Meneland::Execute()
 	while (!quit)
 	{
 		// Ruch
-		if (keystate[SDLK_UP])
+		/*if (keystate[SDLK_UP])
 			player->Move(Up);
 		else if (keystate[SDLK_DOWN])
 			player->Move(Down);
 		else if (keystate[SDLK_LEFT])
 			player->Move(Left);
 		else if (keystate[SDLK_RIGHT])
-			player->Move(Right);
+			player->Move(Right);*/
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_KEYDOWN)
+			{
+				switch (event.key.keysym.sym)
+				{
+					case SDLK_DOWN: moving = true; dir = Down; break;
+					case SDLK_UP: moving = true; dir = Up; break;
+					case SDLK_RIGHT: moving = true; dir = Right; break;
+					case SDLK_LEFT: moving = true; dir = Left; break;
+				}
+			}
+			else if (event.type == SDL_KEYUP)
+			{
+				int sym = event.key.keysym.sym;
+				if (sym == SDLK_DOWN || sym == SDLK_UP || sym == SDLK_LEFT || sym == SDLK_RIGHT)
+					moving = false;
+			}
+		}
+
+		if (moving) player->Move(dir);
 
 		camera->Show();
 
